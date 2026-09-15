a68bbe61bc0d14521025d985455b6fc9199aeea3
KEVMENEA <meneakev@gamil.com>
2026-09-15 19:28:56 +0700

    test config ghcr1

--- Jenkinsfile/testing-pipeline-ghcr.groovy
+++ Jenkinsfile/testing-pipeline-ghcr.groovy
@@ -1,10 +1,8 @@
 stage('Test Production SSH') {
-    steps {
-        sshagent(credentials: ['ecommerce-prod-ssh']) {
-            sh '''
-                ssh -o StrictHostKeyChecking=no meneakev@34.97.42.240 \
-                  "hostname && whoami && docker --version"
-            '''
-        }
+    sshagent(credentials: ['ecommerce-prod-ssh']) {
+        sh '''
+            ssh -o StrictHostKeyChecking=no meneakev@34.97.42.240 \
+            "hostname && whoami && docker --version"
+        '''
     }
 }
