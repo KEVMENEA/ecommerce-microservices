cb70b7531c670e15ae5f28a7dfe6fb95554158e1
KEVMENEA <meneakev@gamil.com>
2026-09-15 21:10:55 +0700

    deploy product block

--- Jenkinsfile/testing-pipeline-ghcr.groovy
+++ Jenkinsfile/testing-pipeline-ghcr.groovy
@@ -6,39 +6,39 @@ node {
                         variable: 'SSH_KEY'
                 )
         ]) {
-            sh '''
-                chmod 600 "$SSH_KEY"
+            sh """
+                chmod 600 "\$SSH_KEY"
 
-                echo "Deploying to ecommerce-prod..."
+                echo "Deploying Jenkins build ${BUILD_NUMBER} to ecommerce-prod..."
 
-                ssh -i "$SSH_KEY" \
+                ssh -i "\$SSH_KEY" \
                     -o IdentitiesOnly=yes \
                     -o StrictHostKeyChecking=no \
                     meneakev@34.97.42.240 \
                     'cd /opt/ecommerce &&
-                     docker compose --env-file .env.prod -f docker-compose.prod.yml pull &&
-                     docker compose --env-file .env.prod -f docker-compose.prod.yml up -d'
+                     IMAGE_TAG=${BUILD_NUMBER} docker compose --env-file .env.prod -f docker-compose.prod.yml pull &&
+                     IMAGE_TAG=${BUILD_NUMBER} docker compose --env-file .env.prod -f docker-compose.prod.yml up -d'
 
-                echo "Production deployment command completed."
-            '''
+                echo "Production build ${BUILD_NUMBER} deployment completed."
+            """
         }
     }
 
     stage('Production Health Check') {
         sh '''
-        echo "Waiting for production services to stabilize..."
-        sleep 30
+            echo "Waiting for production services to stabilize..."
+            sleep 30
 
-        echo "Checking API Gateway health..."
+            echo "Checking API Gateway health..."
 
-        curl --fail --silent --show-error \
-            --retry 5 \
-            --retry-delay 10 \
-            --retry-all-errors \
-            https://ecommerce.meneakevit.store/actuator/health
+            curl --fail --silent --show-error \
+                --retry 5 \
+                --retry-delay 10 \
+                --retry-all-errors \
+                https://ecommerce.meneakevit.store/actuator/health
 
-        echo ""
-        echo "Production health check passed."
-    '''
+            echo ""
+            echo "Production health check passed."
+        '''
     }
 }
