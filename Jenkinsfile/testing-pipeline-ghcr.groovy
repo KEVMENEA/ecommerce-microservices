de0de8e0ac8923f1e01196d13ef46df4b8599d5c
KEVMENEA <meneakev@gamil.com>
2026-09-15 19:32:39 +0700

    test config ghcr3

--- Jenkinsfile/testing-pipeline-ghcr.groovy
+++ Jenkinsfile/testing-pipeline-ghcr.groovy
@@ -1,18 +1,20 @@
-stage('Test Production SSH') {
-    withCredentials([
-            sshUserPrivateKey(
-                    credentialsId: 'ecommerce-prod-ssh',
-                    keyFileVariable: 'SSH_KEY',
-                    usernameVariable: 'SSH_USER'
-            )
-    ]) {
-        sh '''
-            chmod 600 "$SSH_KEY"
+node {
+    stage('Test Production SSH') {
+        withCredentials([
+                sshUserPrivateKey(
+                        credentialsId: 'ecommerce-prod-ssh',
+                        keyFileVariable: 'SSH_KEY',
+                        usernameVariable: 'SSH_USER'
+                )
+        ]) {
+            sh '''
+                chmod 600 "$SSH_KEY"
 
-            ssh -i "$SSH_KEY" \
-                -o StrictHostKeyChecking=no \
-                "$SSH_USER@34.97.42.240" \
-                "hostname && whoami && docker --version"
-        '''
+                ssh -i "$SSH_KEY" \
+                    -o StrictHostKeyChecking=no \
+                    "$SSH_USER@34.97.42.240" \
+                    "hostname && whoami && docker --version"
+            '''
+        }
     }
 }
